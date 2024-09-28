5d5254fdd531efc89ac4695d15ae2411eda1227a
paras <parassuvagiya987@gmail.com>
2024-09-28 19:08:49 +0530

    old ref removed

--- ats_system/AtsUser/admin.py
+++ ats_system/AtsUser/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(ATSUser)
-admin.site.register(UserAccessLevel)
 admin.site.register(Organization)
 admin.site.register(Client)
 admin.site.register(JobSeeker)
