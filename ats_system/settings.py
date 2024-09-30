94c4c62f27491175ad90da0d859af6f60894e3b8
udaysk3 <burluudaysantoshkumar3@gmail.com>
2024-09-30 12:44:43 +0530

    Configured for Deployment

--- ats_system/settings.py
+++ ats_system/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-89vw)a!-=%_^ebt2p6z%&=k5kq(8k%+zi+kaqrmo#k#9dd#sl$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
